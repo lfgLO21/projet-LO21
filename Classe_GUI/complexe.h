1ef951838b092f1a1bc14bd9b800bdbc06a351d8
richard warlock <lookingforgroupLO21@gmail.com>
2012-06-10 02:17:08 +0300

    ajout attribut et méthode de la classe complexe

--- Classe_GUI/complexe.h
+++ Classe_GUI/complexe.h
@@ -1,10 +1,57 @@
 #ifndef COMPLEXE_H
 #define COMPLEXE_H
 
-class Complexe
+#include <cmath>
+#include <exception>
+#include <iostream>
+#include "constante.h"
+#include "operateur.h"
+#include "reel.h"
+#include "rationnel.h"
+#include "entier.h"
+
+class Complexe : public Constante
 {
-public:
-    Complexe();
+    private:
+        Constante*_r;
+        Constante*_i;
+
+    public:
+        Complexe();
+        Complexe(const Complexe & c);
+        Complexe(const Constante& c);
+        Complexe(const Constante* r, const Constante* i);
+        ~Complexe();
+
+        Constante* getR() const;
+        Constante* getI() const;
+
+        void setR(const Constante& r);
+        void setI(const Constante& i);
+
+        void affiche(std::ostream & os = std::cout) const;
+
+        Constante * operator+(const Constante & c) const;
+        Constante * operator-(const Constante & c) const;
+        Constante * operator*(const Constante & c) const;
+        Constante * operator/(const Constante & c) const;
+        Constante * operator-()const;
+
 };
+
+std::ostream & operator<<(std::ostream & os, const Complexe & c);
+
+class ComplexeException
+{
+    private:
+        int _numero;
+        std::string _info;
+    public:
+        ComplexeException(int n, std::string const & info) throw();
+        virtual ~ComplexeException() throw();
+
+        virtual const char* what() const throw();
+};
+
 
 #endif // COMPLEXE_H
