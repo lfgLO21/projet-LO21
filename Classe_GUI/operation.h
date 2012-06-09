455a27d94f9eef1114f788d76f9694ace330210d
richard warlock <lookingforgroupLO21@gmail.com>
2012-06-10 01:50:43 +0300

    ajout des operations

--- Classe_GUI/operation.h
+++ Classe_GUI/operation.h
@@ -2,13 +2,88 @@
 #define OPERATION_H
 
 #include "constante.h"
-#include "entier.h"
 #include "reel.h"
+#include "entier.h"
+#include "rationnel.h"
+#include <cmath>
 
 class Operation
 {
-    public:
-        Operation();
+public:
+    Operation();
 };
+
+struct Addition : public Operation{
+  static Constante* application(const Constante&, const Constante&);
+};
+
+struct Soustraction : public Operation{
+  static Constante* application(const Constante&, const Constante&);
+};
+
+struct Multiplication : public Operation{
+  static Constante* application(const Constante&, const Constante&);
+};
+
+struct Division : public Operation{
+  static Constante* application(const Constante&, const Constante&);
+};
+
+struct POW : public Operation{
+  static Constante* application(const Constante&, const Constante&);
+};
+
+struct SIGN : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct SQUARE : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct CUBE : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct INV : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct SQRT : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct COS : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct COSH : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct SIN : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct SINH : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct TAN : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct TANH : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct LOG : public Operation{
+  static Constante* application(const Constante&);
+};
+
+struct LN : public Operation{
+  static Constante* application(const Constante&);
+};
+
 
 #endif // OPERATION_H
