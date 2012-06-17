3199e69fd1ca0fe4488f11c0c393a67b2c324cc9
Richard Warlock <lookingforgroupLO21@gmail.com>
2012-06-17 19:41:22 +0200

    ajout doc dioxygen

--- Classe_GUI/rationnel.h
+++ Classe_GUI/rationnel.h
@@ -18,30 +18,96 @@ public:
 
 };
 
+/*! \class Rationnel
+  * \brief class représentant les constantes qui sont des rationnels
+  *
+  * La classe comporte deux attribut integer
+  */
 class Rationnel:public Constante
 {
-    int _numerateur;
-    int _denominateur;
+    int _numerateur; /*! valeur du numérateur */
+    int _denominateur; /*! valeur du dénominateur */
 
     //méthode privée
+    /*!
+      * \brief simplification
+      * Méthode privé permettante de simplifier quand celà est possible la fraction manipulé
+      */
     void simplification();
 
 public:
+
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Rationnel
+      * \param n : valeur de l'attribut _numerateur
+      * \param d : valeur de l'attribut _denominateur
+      */
     Rationnel(int n=0, int d=1);
+
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Rationnel
+      * \param e : référence sur un Entier existant
+      */
     Rationnel(const Entier& e);
+
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Rationnel
+      * \param n : valeur de l'attribut _numerateur à partir d'un entier existant
+      * \param d : valeur de l'attribut _denominateur à partir d'un entier existant
+      */
     Rationnel(const Entier& n, const Entier& d);
+
+    /*!
+      * \brief Constructeur de copie
+      * Constructeur de la classe Rationnel à partir d'un Rationnel existant déjà
+      * \param r : référence sur un Rationnel existant
+      */
     Rationnel(const Rationnel& r);
 
+    /*!
+      * \brief Destructeur
+      * Destructeur de la classe Rationnel
+      */
     ~Rationnel(){}
 
     //get
+    /*!
+      * \brief getNumerateur
+      * Méthode renvoyant la valeur de l'attribut _numerateur de la classe
+      * \return _numerateur
+      */
     int getNumerateur()const;
+
+    /*!
+      * \brief getDenominateur
+      * Méthode renvoyant la valeur de l'attribut _denominateur de la classe
+      * \return _denominateur
+      */
     int getDenominateur()const;
 
     //set
+    /*!
+      * \brief setNumerateur
+      * Méthode modifiant la valeur de l'attribut _numerateur de la classe
+      * \param  n : nouvel valeur de _numerateur
+      */
     void setNumerateur(int n);
+
+    /*!
+      * \brief setDenominateur
+      * Méthode modifiant la valeur de l'attribut _denominateur de la classe
+      * \param  e : nouvel valeur de _denominateur
+      */
     void setDenominateur(int d);
 
+    /*!
+      * \brief toString
+      * Methode permettant de transformer le rationnel manipulé en une chaine de caractère
+      * \return string
+      */
     std::string toString()const{
         std::stringstream ss;
         ss<<_numerateur<<"/"<<_denominateur;
@@ -49,16 +115,67 @@ public:
     }
 
     //affiche
+    /*!
+      * \brief affiche
+      * Methode permettant un affichage du rationnel manipulé
+      * \param os
+      */
     void affiche(std::ostream & os = std::cout) const;
 
     //operateur
+    /*!
+      * \brief operator+
+      * Methode retournant la somme du rationnel manipulé avec la constante passé en argument
+      * \param c
+      * \return Constante*
+      */
     Constante * operator+(const Constante & c) const;
+
+    /*!
+      * \brief operator-
+      * Methode retournant la différence du rationnel manipulé avec la constante passé en argument
+      * \param c
+      * \return Constante*
+      */
     Constante * operator-(const Constante & c) const;
+
+    /*!
+      * \brief operator*
+      * Methode retournant le produit du rationnel manipulé avec la constante passé en argument
+      * \param c
+      * \return Constante*
+      */
     Constante * operator*(const Constante & c) const;
+
+    /*!
+      * \brief operator/
+      * Methode retournant la division du rationnel manipulé avec la constante passé en argument
+      * \param c
+      * \return Constante*
+      */
     Constante * operator/(const Constante & c) const;
+
+    /*!
+      * \brief operator-
+      * Methode retournant le contraire du rationnel manipulé
+      * \return Constante*
+      */
     Constante * operator-()const;
 
+    /*!
+      * \brief operator==
+      * Methode vérifiant si le rationnel manipulé est égal à la constante passé en argument
+      * \param c
+      * \return true si il y a égalité, false sinon
+      */
     bool operator==(const Constante & c) const;
+
+    /*!
+      * \brief operator<
+      * Methode vérifiant si le rationnel manipulé est inférieur à la constante passé en argument
+      * \param c
+      * \return true si la constante manipulé est inférieur, false sinon
+      */
     bool operator<(const Constante & c) const;
 
 };
--- Classe_GUI/reel.h
+++ Classe_GUI/reel.h
@@ -6,24 +6,71 @@
 #include "expression.h"
 #include "rationnel.h"
 
+/*! \class Reel
+  * \brief classe représentant les constantes qui sont des réels
+  *
+  * La classe comporte un attribut float
+  */
 class Reel : public Constante
 {
-    float _reel;
+    float _reel; /*! valeur du reel */
 
 public:
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Reel
+      * \param r : valeur de l'attribut _reel
+      */
     Reel(float r = 0);
+
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Reel
+      * \param e : référence sur un Entier existant
+      */
     Reel(const Entier& e);
+
+    /*!
+      * \brief Constructeur
+      * Constructeur de la classe Reel
+      * \param r : référence sur un Rationnel existant
+      */
     Reel(const Rationnel& r);
+
+    /*!
+      * \brief Constructeur de copie
+      * Constructeur de la classe Reel à partir d'un Reel existant déjà
+      * \param e : référence sur un Reel existant
+      */
     Reel(const Reel& e);
 
+    /*!
+      * \brief Destructeur
+      * Destructeur de la classe Reel
+      */
     ~Reel(){}
 
     //get
+    /*!
+      * \brief getReel
+      * Méthode renvoyant la valeur de l'attribut _reel de la classe
+      * \return _reel
+      */
     float getReel() const;
 
     //set
+    /*!
+      * \brief setReel
+      * Méthode modifiant la valeur de l'attribut _reel de la classe
+      * \param  r : nouvel valeur de _reel
+      */
     void setReel(float r);
 
+    /*!
+      * \brief toString
+      * Methode permettant de transformer le réel manipulé en une chaine de caractère
+      * \return string
+      */
     std::string toString()const{
         std::stringstream ss;
         ss << _reel;
@@ -34,13 +81,59 @@ public:
     void affiche(std::ostream & os = std::cout) const;
 
     //operateur
+    /*!
+      * \brief operator+
+      * Methode retournant la somme du Reel manipulé avec la constante passé en argument
+      * \param r
+      * \return Constante*
+      */
     Constante * operator+(const Constante & r) const;
+
+    /*!
+      * \brief operator-
+      * Methode retournant la différence du Reel manipulé avec la constante passé en argument
+      * \param r
+      * \return Constante*
+      */
     Constante * operator-(const Constante & r) const;
+
+    /*!
+      * \brief operator*
+      * Methode retournant le produit du Reel manipulé avec la constante passé en argument
+      * \param r
+      * \return Constante*
+      */
     Constante * operator*(const Constante & r) const;
+
+    /*!
+      * \brief operator/
+      * Methode retournant la division du Reel manipulé avec la constante passé en argument
+      * \param r
+      * \return Constante*
+      */
     Constante * operator/(const Constante & r) const;
+
+    /*!
+      * \brief operator-
+      * Methode retournant le contraire du Reel manipulé
+      * \return Constante*
+      */
     Constante * operator-()const;
 
+    /*!
+      * \brief operator==
+      * Methode vérifiant si le Reel manipulé est égal à la constante passé en argument
+      * \param c
+      * \return true si il y a égalité, false sinon
+      */
     bool operator==(const Constante & c) const;
+
+    /*!
+      * \brief operator<
+      * Methode vérifiant si le Reel manipulé est inférieur à la constante passé en argument
+      * \param c
+      * \return true si la constante manipulé est inférieur, false sinon
+      */
     bool operator<(const Constante & c) const;
 
 };
